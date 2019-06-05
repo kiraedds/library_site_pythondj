38976ff15afe5d6443211c7ddef5359cd821f41f
Bartłomiej Zięba <b.zieba.vh@gmail.com>
2019-06-05 21:59:19 +0200

    Trying to figure out how to edit existing book, failed

--- LibraryProject/books/views.py
+++ LibraryProject/books/views.py
@@ -43,6 +43,68 @@ def details(request, book_id):
     return render(request, 'books/details.html', context)
 
 
+def delete_book(request, book_id):
+    try:
+        book = Book.objects.get(id=book_id)
+    except Book.DoesNotExist:
+        raise Http404("Book does not exist")
+    book.delete()
+    all_books = Book.objects.all()
+    context = {
+        'all_books': all_books,
+    }
+    return render(request, 'books/index.html', context)
+
+
+def edit_book(request, book_id):
+    print("edit")
+    try:
+        book = Book.objects.get(id=book_id)
+    except Book.DoesNotExist:
+        raise Http404("Book does not exist")
+
+    form = BookForm(initial={
+        'title': book.title,
+        'author': book.author,
+        'ISBN': book.ISBN,
+        'year': book.year,
+        'genre': book.genre,
+        'publisher': book.publisher,
+        'cover': book.cover,
+        'numberOfAvailable': book.numberOfAvailable,
+        'numberOfAll': book.numberOfAll,
+    })
+
+    context = {
+        'form': form,
+    }
+
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+
+        if form.is_valid():
+            title = request.POST.get('title', )
+            ISBN = request.POST.get('ISBN', )
+            genre = request.POST.get('genre', )
+            author_id = request.POST.get('author', )
+            author = Author.objects.get(id=author_id)
+            publisher_id = request.POST.get('publisher', )
+            publisher = Publisher.objects.get(id=publisher_id)
+            year = request.POST.get('year', )
+            numberOfAvailable = request.POST.get('numberOfAvailable', )
+            numberOfAll = request.POST.get('numberOfAll', )
+            book_obj = Book(title=title, ISBN=ISBN, genre=genre, author=author, publisher=publisher,
+                            year=year, numberOfAvailable=numberOfAvailable, numberOfAll=numberOfAll)
+            book_obj.save()
+            # book.delete()
+            all_books = Book.objects.all()
+            context = {
+                'all_books': all_books,
+            }
+        return render(request, 'books/index.html', context)
+    return render(request, 'books/edit_book/' + book_id, context)
+
+
 def create_book(request):
     form = BookForm()
     context = {'form': form}
