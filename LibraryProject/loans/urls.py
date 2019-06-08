6ab1801e0e86c5de8111be4071e51bbaf15ad53f
Bartłomiej Zięba <b.zieba.vh@gmail.com>
2019-06-08 16:20:28 +0200

    Minor fixes

--- LibraryProject/loans/urls.py
+++ LibraryProject/loans/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('user_loans', views.user_loans),
     url(r'^(?P<loan_id>[0-9]+)/$', views.details),
     path('return_book/<loan_id>', views.return_book),
 ]
